5daa2c41cede18c0585886591126dcf4b7af5db4
siu michael <siu.michael1@gmail.com>
2025-10-02 14:50:24 +0000

    it is now directing me to film on the computer itself, but i want to gen

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,19 +1,27 @@
+"use client"
+
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(false)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    const checkDevice = () => {
+      const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+      const isNarrowScreen = window.innerWidth < MOBILE_BREAKPOINT;
+      // A device is considered mobile if it has touch AND a narrow screen,
+      // or just has touch (covers iPads etc. that aren't narrow)
+      // This is a more reliable check than just screen width.
+      setIsMobile(isTouchDevice || isNarrowScreen);
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    checkDevice(); // Check on initial mount
+
+    window.addEventListener("resize", checkDevice)
+    return () => window.removeEventListener("resize", checkDevice)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }

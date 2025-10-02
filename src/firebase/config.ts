a5fd313321db33e2ff89fb3a4aa62561191d2526
siubbbb <128900876+siubbbb@users.noreply.github.com>
2025-10-02 17:44:27 +0100

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7863473142-66aa8",
-  "appId": "1:1057106703231:web:17d37093617f5ad78ca04a",
-  "apiKey": "AIzaSyB2SISt9o1jTrOad78EPHw-ZRBNeeiTJTc",
-  "authDomain": "studio-7863473142-66aa8.firebaseapp.com",
+  "projectId": "",
+  "appId": "",
+  "apiKey": "",
+  "authDomain": "",
   "measurementId": "",
-  "messagingSenderId": "1057106703231"
+  "messagingSenderId": ""
 };
